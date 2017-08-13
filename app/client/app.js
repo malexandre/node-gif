94ef312fa5e7883f270342481f35474d07ea0db9
Marc Alexandre <marc@lumapps.com>
2017-08-14 00:46:50 +0200

    Create search bar module

--- app/client/app.js
+++ app/client/app.js
@@ -4,15 +4,27 @@ import { BrowserRouter } from 'react-router-dom'
 import Styles from './styles.scss'
 
 import NavBar from './modules/navbar'
+import SearchBar from './modules/search-bar'
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filter: ''
+        };
+
+        this.onFilterSubmit = this.onFilterSubmit.bind(this);
+    }
+
+    onFilterSubmit(filter) {
+        this.setState({ filter })
+    }
+
     render() {
         return (
             <div className="container">
                 <NavBar />
-                <div className="jumbotron">
-                    <h1 className="display-3">Hello, world!</h1>
-                </div>
+                <SearchBar onFilterSubmit={ this.onFilterSubmit } />
             </div>
         )
     }
---
+++ app/client/modules/search-bar/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            filter: ''
+        };
+
+        this.onFilterSubmit = this.onFilterSubmit.bind(this);
+    }
+
+    onFilterSubmit(event) {
+        event.preventDefault()
+
+        if (this.props.onFilterSubmit) {
+            this.props.onFilterSubmit(this.state.filter)
+        }
+    }
+
+    render() {
+        return (
+            <div className="row my-3">
+                <div className="col-lg-12">
+                    <form onSubmit={ this.onFilterSubmit }>
+                        <div className="input-group">
+                            <input type="text"
+                                   className="form-control"
+                                   placeholder="Search for..."
+                                   aria-label="Search for..."
+                                   value={ this.state.filter }
+                                   onChange={ (event) => this.setState({ filter: event.target.value }) } />
+                            <span className="input-group-btn">
+                                <button className="btn btn-primary" type="submit">
+                                    Go!
+                                </button>
+                            </span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchBar
