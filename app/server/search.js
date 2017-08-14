a2fb6b3925a03ac111b6ae099f4baaafdc64edd2
Marc Alexandre <marc@lumapps.com>
2017-08-14 13:34:27 +0200

    Refactor search with async/await

--- app/server/search.js
+++ app/server/search.js
@@ -1,30 +1,39 @@
 const config = require('./config')
 const emojilib = require('emojilib')
 const giphy = require('giphy-api')(config.GIPHY_API_KEY)
+const winston = require('winston')
 
 module.exports = {}
 
-giphySearch = (query, offset, callback) => {
-    giphy.search({
-        q: query,
-        limit: 20,
-        offset: offset
-    }).then(resGiphy => {
-        callback({
-            items: resGiphy.data,
-            more: resGiphy.pagination.total_count > resGiphy.pagination.count + resGiphy.pagination.offset
+const giphySearch = async (query, offset) => {
+    let resGiphy
+    try {
+        resGiphy = await giphy.search({
+            q: query,
+            limit: 20,
+            offset: offset
         })
-    }).catch(reason => console.error(reason))
+        // console.log(resGiphy)
+    }
+    catch (err) {
+        winston.log('error', 'Error from Giphy Api.', err)
+        return
+    }
+
+    return {
+        items: resGiphy.data,
+        more: resGiphy.pagination.total_count > resGiphy.pagination.count + resGiphy.pagination.offset
+    }
 }
 
-emojiSearch = (query, offset, callback) => {
+const emojiSearch = (query, offset) => {
     let emojis = []
 
-    Object.keys(emojilib.lib).forEach(key => {
+    Object.keys(emojilib.lib).forEach((key) => {
         let match = key.includes(query)
 
         if (!match) {
-            match = emojilib.lib[key].keywords.find(word => word.includes(query)) !== undefined
+            match = emojilib.lib[key].keywords.find((word) => word.includes(query)) !== undefined
         }
 
         if (match) {
@@ -32,14 +41,14 @@ emojiSearch = (query, offset, callback) => {
         }
     })
 
-    callback({
+    return {
         items: emojis.splice(offset, 20),
         more: emojis.splice(offset, 21).length > 20
-    })
+    }
 }
 
-module.exports.searchHandler = (req, res) => {
-    let offset = parseInt(req.query.offset) || 0
+module.exports.searchHandler = async (req, res) => {
+    let offset = parseInt(req.query.offset) || 0
     let nextPage = `/api/${req.params.type}/search/${req.params.query}?offset=${offset + 20}`
 
     let searchFn = () => { res.sendStatus(404) }
@@ -51,8 +60,15 @@ module.exports.searchHandler = (req, res) => {
         searchFn = emojiSearch
     }
 
-    searchFn(req.params.query, offset, ({ items, more }) => res.json({
-        items: items,
-        nextPage: more ? nextPage : undefined
-    }))
+    const results = await searchFn(req.params.query, offset)
+
+    if (!results) {
+        res.sendStatus(500)
+        return
+    }
+
+    res.json({
+        items: results.items,
+        nextPage: results.more ? nextPage : undefined
+    })
 }
