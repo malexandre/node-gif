cc2b1786d7a5c0ecc16d84dd489c8375ff9c1085
Marc Alexandre <marc@lumapps.com>
2017-08-14 15:27:32 +0200

    Edit style for GifList

--- app/client/modules/gif-list/gif-item.js
+++ app/client/modules/gif-list/gif-item.js
@@ -1,24 +1,20 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-import './styles/gif-item.scss'
-
 const GifItem = (props) => {
     return (
         <div className="col-lg-3">
-            <div className="card mb-3">
-                <img className="card-img-top gif-item_fix-height-image_img"
+            <div className="card mb-3 text-center">
+                <img className="card-img-top"
                     src={ props.gif.images.preview_webp.url }
                     alt={ `Preview gif ${props.gif.slug}` } />
                 <div className="card-body">
-                    <div className="row">
-                        <button className="btn btn-sm btn-warning">
-                            Favorite
-                        </button>
-                        <a className="btn btn-sm btn-secondary" href={ props.gif.url }>
-                            Open in Giphy
-                        </a>
-                    </div>
+                    <button className="btn btn-sm btn-warning mr-3">
+                        Favorite
+                    </button>
+                    <a className="btn btn-sm btn-secondary" href={ props.gif.url }>
+                        Open in Giphy
+                    </a>
                 </div>
             </div>
         </div>
