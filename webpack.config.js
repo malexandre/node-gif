c462272ba3150beaf5f52997c023a1fbae8cf49f
Marc Alexandre <marc@lumapps.com>
2017-08-17 14:16:17 +0200

    Reformat webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,54 +10,56 @@ module.exports = {
         filename: 'app.min.js'
     },
     module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['es2015', 'react']
-            }
-        }, {
-            test: /\.(scss)$/,
-            use: [{
-                loader: 'style-loader',
-            }, {
-                loader: 'css-loader',
-            }, {
-                loader: 'postcss-loader',
-                options: {
-                    plugins: function () {
-                        return [
-                            require('precss'),
-                            require('autoprefixer')
-                        ];
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function() {
+                                return [require('precss'), require('autoprefixer')]
+                            }
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
                     }
-                }
-            }, {
-                loader: 'sass-loader'
-            }]
-        }]
+                ]
+            }
+        ]
     },
     plugins: [
-        new HtmlWebpackPlugin({ 'template': './app/client/index.ejs' }),
+        new HtmlWebpackPlugin({ template: './app/client/index.ejs' }),
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
             'window.jQuery': 'jquery',
             Popper: ['popper.js', 'default'],
-            Tether: "tether",
-            "window.Tether": "tether",
-            Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
-            Button: "exports-loader?Button!bootstrap/js/dist/button",
-            Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
-            Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
-            Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
-            Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
-            Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
-            Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
-            Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
-            Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
-            Util: "exports-loader?Util!bootstrap/js/dist/util",
+            Tether: 'tether',
+            'window.Tether': 'tether',
+            Alert: 'exports-loader?Alert!bootstrap/js/dist/alert',
+            Button: 'exports-loader?Button!bootstrap/js/dist/button',
+            Carousel: 'exports-loader?Carousel!bootstrap/js/dist/carousel',
+            Collapse: 'exports-loader?Collapse!bootstrap/js/dist/collapse',
+            Dropdown: 'exports-loader?Dropdown!bootstrap/js/dist/dropdown',
+            Modal: 'exports-loader?Modal!bootstrap/js/dist/modal',
+            Popover: 'exports-loader?Popover!bootstrap/js/dist/popover',
+            Scrollspy: 'exports-loader?Scrollspy!bootstrap/js/dist/scrollspy',
+            Tab: 'exports-loader?Tab!bootstrap/js/dist/tab',
+            Tooltip: 'exports-loader?Tooltip!bootstrap/js/dist/tooltip',
+            Util: 'exports-loader?Util!bootstrap/js/dist/util'
         })
     ]
 }
